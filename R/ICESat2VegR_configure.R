0b351bd077abc0dff21c26d8e9c14ab130d0c2db
alexandergaskins <gaskins.alex@yahoo.com>
2026-05-26 10:13:55 -0400

    fix: add missing ICESat2VegR_configure export to ICESat2VegR_configure.R

--- R/ICESat2VegR_configure.R
+++ R/ICESat2VegR_configure.R
@@ -238,3 +238,13 @@ tryInitializeCloudCapabilities <- function() {
 
   invisible(TRUE)
 }
+
+#' Configure ICESat2VegR Python dependencies
+#'
+#' Installs the required Python packages (earthaccess, h5py, earthengine-api)
+#' used by ICESat2VegR via reticulate.
+#'
+#' @export
+ICESat2VegR_configure <- function() {
+  reticulate::py_install(c("earthaccess", "h5py", "earthengine-api"))
+}
