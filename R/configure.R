68a5a6ea5beb4f1be32635efa63bf2f902f6f3fa
alexandergaskins <gaskins.alex@yahoo.com>
2026-05-26 10:14:23 -0400

    fix: remove redundant configure.R

--- R/configure.R
+++
@@ -1,9 +0,0 @@
-#' Configure ICESat2VegR Python dependencies
-#'
-#' Installs the required Python packages (earthaccess, h5py, earthengine-api)
-#' used by ICESat2VegR via reticulate.
-#'
-#' @export
-ICESat2VegR_configure <- function() {
-  reticulate::py_install(c("earthaccess", "h5py", "earthengine-api"))
-}
